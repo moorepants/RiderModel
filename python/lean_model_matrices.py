765556b1bdfe0a424b2ce6c6695cab4145f61bdb
Jason Moore <moorepants@gmail.com>
2010-07-09 10:40:02 -0700

    producing some fine ab matrices now

---
+++ python/lean_model_matrices.py
@@ -0,0 +1,26 @@
+from RiderLeanAB import RiderLeanAB
+import pickle
+
+ppdir = '/media/Data/Documents/School/TU Delft/PhysicalParameters/'
+
+# load in the base data file
+f = open(ppdir + 'data/data.p', 'r')
+data = pickle.load(f)
+f.close()
+
+v = [2.5, 5., 7.5]
+# write the parameter files
+for i, name in enumerate(data['bikes']):
+    fname = ''.join(name.split())
+    directory = '../data/BikeLeanAB/'
+    f = open(directory + fname + 'LeanAB.txt', 'w')
+    f.write('States (x) are: [Roll,Steer,Lean,Roll Rate,Steer Rate,Lean Rate]\n')
+    f.write('Inputs (u) are: [Roll Torque,Steer Torque,Lean Torque]\n')
+    for speed in v:
+        A, B, nothing = RiderLeanAB(i, speed)
+        print A[-1,1]
+        f.write('A' + ' (v = ' + str(speed) + ')\n')
+        f.write(str(A) + '\n')
+        f.write('B' + ' (v = ' + str(speed) + ')\n')
+        f.write(str(B) + '\n')
+    f.close()
